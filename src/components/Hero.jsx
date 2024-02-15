80d6bbf57352807f8b252c4e7e531781800fafc4
Pratik doke <pratikdoke7038@gmail.com>
2024-02-15 09:18:35 +0530

    third commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,23 @@
-
+// eslint-disable-next-line no-unused-vars
+import Logo from './Image/cart.png'
 import './style/style.css'
+import heroBackgroundImage from './Image/image.png'; // Corrected import path for background image
+import logoImage from './Image/logo.png'; // Corrected import path for logo image
+import groceryImage from './Image/grocery-image.png'; // Corrected import path for grocery image
+
 const Hero = () => {
   return (
-    <div className="container px-28 py-5 bg-center  bg-cover min-h-screen min-w-full" style={{ backgroundImage: `url('./src/components/image/image.png')` }}>
+    <div className="container px-28 py-5 bg-center  bg-cover min-h-screen min-w-full" style={{ backgroundImage: `url(${heroBackgroundImage})` }}>
       <div>
         <nav className="flex items-center">
-          <img src="./src/components/image/logo.png" alt="Logo" className="w-40 cursor-pointer" />
+          <img src={logoImage} alt="Logo" className="w-40 cursor-pointer" />
           <ul className="flex-1 text-center">
             <li className="list-none inline-block px-3"><a className="no-underline text-white px-2" href="#">Home</a></li>
             <li className="list-none inline-block px-3"><a className="no-underline text-white px-2" href="#">About</a></li>
             <li className="list-none inline-block px-3"><a className="no-underline text-white px-2" href="#">Features</a></li>
             <li className="list-none inline-block px-3"><a className="no-underline text-white px-2" href="#">Contact</a></li>
           </ul>
-          <img src="src\components\image\cart.png" alt="Cart" className="w-8 cursor-pointer" />
+          <img src={Logo} alt="Cart" className="w-8 cursor-pointer" />
         </nav>
       </div>
       <div className="text-white mt-48 max-w-xl">
@@ -23,7 +28,7 @@ const Hero = () => {
           <a href="#" className="text-white">Download App <span className="text-lg inline-block rotate-90">&#10148;</span> </a>
         </div>
       </div>
-      <img src="./src/components/image/grocery-image.png" className="w-full md:w-1/2 md:absolute bottom-0 right-20" alt="" />
+      <img src={groceryImage} className="w-full md:w-1/2 md:absolute bottom-0 right-20" alt="" />
     </div>
   );
 };
